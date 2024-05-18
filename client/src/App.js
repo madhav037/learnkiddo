49b4bc63c9a9dc665c867ca6f76260b1a7a477dc
hetvideshani <hetvideshani@gmail.com>
2024-05-18 12:34:33 +0530

    herosection

--- client/src/App.js
+++ client/src/App.js
@@ -17,11 +17,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Herosection />}></Route>
           <Route path='/signin' element={<Signin />}></Route>
-<<<<<<< HEAD
           <Route path='/dashboard' element={<Dashboard />}></Route>
-=======
           <Route path='/home' element={<Dashboard />}></Route>
->>>>>>> b8bb2dc7a467b6ca0a24f23df9907ff993ed1ed0
           <Route path='/signup' element={<Signup />}></Route>
           <Route path='/videoHome' element={<VideoDashboard />}></Route>
           <Route path='/videoHome/:id' element={<InnerVideo />}></Route>
