1fbda36f563ec4b6f372413a489e2b9a45a0474b
hetvideshani <hetvideshani@gmail.com>
2024-05-18 12:30:06 +0530

    herosection

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
+import Herosection from './components/Herosection';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Herosection />}></Route>
           <Route path='/signin' element={<Signin />}></Route>
-          <Route path='/' element={<Dashboard />}></Route>
+          <Route path='/dashboard' element={<Dashboard />}></Route>
           <Route path='/signup' element={<Signup />}></Route>
         </Routes>
       </BrowserRouter>
